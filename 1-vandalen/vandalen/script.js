d7adebaf4777dc79ca16424a9f09c97ea90758c0
ad222kr <ad222kr@student.lnu.se>
2014-11-07 21:09:17 +0100

    BÖrjat med uppgift 4. Får ut min,max, averageage och names

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,9 +1,58 @@
 "use strict";
 
-var makePerson = function(persArr){
+var makePerson = function (persArr) {
+    var minAge;
+    var maxAge;
+    var averageAge;
+    var names = "";
+    var sumOfAge = 0;
+
+    // Sortera arrayen på ålder
+    persArr.sort(function (a, b) {
+        return a.age-b.age});
+
+    // Tilldela minAge och maxAge
+    minAge = persArr[0].age;
+    maxAge = persArr[persArr.length - 1].age;
+    
+    // Sortera arrayen på namn
+    persArr.sort(function (a, b) {
+        if (a.name < b.name) {
+            return -1;
+        } else if (a.name > b.name) {
+            return 1;
+        } else {
+            return 9;
+        }
+    });
+    console.log(persArr);
+
+    // Tilldela names värden
+    for (var i = 0; i < persArr.length; i++) {
+        names += persArr[i].name + ", ";
+        sumOfAge += persArr[i].age;
+    }
+
+    //averageAge = (persArr[0].age + persArr[1].age + persArr[2].age) / 3;
+    
+    averageAge = sumOfAge / persArr.length;
+    console.log(names);
+    console.log(minAge);
+    console.log(maxAge);
+    console.log(averageAge);
 
 
-	// Din kod här...
 
-}
+    var result = {};
+    result.minAge = minAge;
+    result.maxAge = maxAge;
+    
+    return result; }
+
+
+
+var data = [{ name: "John Häggerud", age: 37 }, { name: "Johan Leitet", age: 36 }, { name: "Mats Loock", age: 46 }];
+
+var result = makePerson(data);
+
 
