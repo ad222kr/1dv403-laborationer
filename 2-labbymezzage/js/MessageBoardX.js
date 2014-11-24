4ffa0624a74064f8aed7054fb8f8da0d71a67add
ad222kr <ad222kr@student.lnu.se>
2014-11-24 12:54:15 +0000

    Started working so i can have multiple instances of MessageBoard on the page

---
+++ 2-labbymezzage/js/MessageBoardX.js
@@ -0,0 +1,127 @@
+"use strict";
+var MessageBoard = {
+    
+    
+    messages: [],
+    
+    init: function(){
+        // Reference to button
+        var button = document.getElementById("button");
+        var text = document.getElementById("textarea");
+        
+        // Add eventhandlers
+        button.addEventListener("click", MessageBoard.createMessage, false);
+        text.addEventListener("keypress", function (e) {
+            if (e.keyCode == 13 && !e.shiftKey){
+                e.preventDefault();
+                MessageBoard.createMessage();
+            }
+        }, false);
+    },
+    
+    messageCount: function(){
+        // Generates messagecounter
+        var count = MessageBoard.messages.length;
+        var div = document.getElementById("numberofmess");
+        var text = document.createElement("p");
+        
+        div.innerHTML = "";
+        text.innerHTML = "Antal meddelanden: " + count;
+        div.appendChild(text);
+    },
+    
+    createMessage: function(){
+        // Creates messageobject from textareas value and date right now
+        var input = document.getElementById("textarea").value;
+        var mess = new Message(input, new Date());
+        
+        // Adds message to array and renders it
+        MessageBoard.messages.push(mess);
+        MessageBoard.renderMessage(MessageBoard.messages.length - 1);
+        MessageBoard.messageCount();
+        
+        // Resets textarea
+        document.getElementById("form").reset();
+        return false;
+    },
+    
+    renderMessage: function(messageID){
+        // Reference to div that holds all messages
+        var div = document.getElementById("messages");
+        
+        // Creating elements for message, text, time and delete/timestamp buttons
+        var messageDiv = document.createElement("div");
+        var buttonDiv = document.createElement("div");
+        var text = document.createElement("p");
+        var time = document.createElement("p");
+        var aClose = document.createElement("a");
+        var aTime = document.createElement("a");
+        var imgClose = document.createElement("img");
+        var imgTime = document.createElement("img");
+        
+        // Setting classnames and attributes
+        buttonDiv.className = "buttons";
+        messageDiv.className = "message";
+        text.className = "msgtext";
+        time.className = "timestamp";
+        imgClose.className = "delete";
+        imgTime.className = "time";
+        aClose.setAttribute("href", "#");
+        aTime.setAttribute("href", "#");
+        imgClose.setAttribute("src", "img/Remove-icon.png");
+        imgTime.setAttribute("src", "img/clock.png");
+        
+        // Pushing elements to DOM
+        div.appendChild(messageDiv);
+        aClose.appendChild(imgClose);
+        aTime.appendChild(imgTime);
+        buttonDiv.appendChild(aClose);
+        buttonDiv.appendChild(aTime);
+        messageDiv.appendChild(buttonDiv);
+        messageDiv.appendChild(text);
+        messageDiv.appendChild(time);
+        
+        // Rendering the message and time
+        text.innerHTML = MessageBoard.messages[messageID].getHTMLtext();
+        time.innerHTML = MessageBoard.messages[messageID].getDateText();
+        
+        // Eventhandler for deleting a message on mouseclick
+        imgClose.alt="Close";
+        imgClose.onclick = function () {
+            MessageBoard.deleteMessage(messageID);
+            MessageBoard.messageCount();
+        };
+        imgTime.alt="Time";
+        imgTime.onclick = function () {
+            MessageBoard.showTimeStamp(messageID);   
+        };
+    },
+    
+    renderMessages: function(){
+        // Resets messages element and renders all messages
+        document.getElementById("messages").innerHTML =""; 
+        
+        for(var i = 0; i < MessageBoard.messages.length; i++){
+            MessageBoard.renderMessage(i);
+        }
+    },
+    
+    deleteMessage: function(messageID){
+        // Deletes one message, then renders all messag
+        var result = window.confirm("Vill du verkligen ta bort meddelandet?");
+        if(result){
+            MessageBoard.messages.splice(messageID, 1);
+            MessageBoard.renderMessages();   
+        }
+    },
+    
+    showTimeStamp: function(messageID){
+        alert(MessageBoard.messages[messageID].toString());    
+    }
+};
+
+window.onload = MessageBoard.init;
+
+
+
+
---
+++ 2-labbymezzage/js/run.js
@@ -0,0 +1,10 @@
+"use strict";
+window.onload = function(){
+    var board1 = new MessageBoard("board1");
+    var board2 =new MessageBoard("board2");
+
+    board1.init();
+    board2.init();
+    
+};
+
