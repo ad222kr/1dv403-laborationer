1aec58f707e5c46cb90498814fd5c18b755574fc
ad222kr <ad222kr@student.lnu.se>
2014-11-24 15:15:46 +0000

    Eventlistener for enter-press, added form to reset the text-area

--- 2-labbymezzage/js/MessageBoard.js
+++ 2-labbymezzage/js/MessageBoard.js
@@ -2,27 +2,41 @@
 function MessageBoard(boardID){
     var that = this;
     var messages = [];
+    var div = document.getElementById(boardID);
     
     this.init = function(){
         // Init function that initiates the board, creating a button and a textfield
         
-        // Reference to the boardID
-        var div = document.getElementById(boardID);
+
+        // Create form to use the form.reset
+        var form = document.createElement("form");
+        form.className = "form";
+        div.appendChild(form)
         
         // Create a text-field
         var textarea = document.createElement("textarea");
         textarea.className = "textarea";
-        div.appendChild(textarea);
+        form.appendChild(textarea);
         
         // Create a send-button
         var inputButton = document.createElement("input");
         inputButton.type = "button";
         inputButton.value = "skicka";
         inputButton.className = "button";
-        div.appendChild(inputButton);
+        form.appendChild(inputButton);
+        
+        
         
         // Add eventlisteners
         inputButton.addEventListener("click", that.createMessage, false);
+        textarea.addEventListener("keypress", function(e) {
+            if (!e) var e = window.event;
+            if (e.keyCode == 13 && !e.shiftKey){
+                e.preventDefault();
+                that.createMessage();
+            }
+            
+        });
     };
     
     this.createMessage = function(){
@@ -30,36 +44,22 @@ function MessageBoard(boardID){
         var mess = new Message(input, new Date());
         
         // Adds message to array and renders it
-        
-        
         messages.push(mess);
-        console.log(messages);
+        console.log(messages.length);
         
+        // Resets the text-area
+        document.getElementsByClassName("form")[0].reset();
     };
+    
+    this.renderMessage = function(){
+                
+        
+        
+    }
 
 
 }
 
-MessageBoard.prototype.asd = function(){
-    
-    
-    
-    // Create send-button
-    var inputButton = document.createElement("input");
-    inputButton.type = "button";
-    inputButton.value = "skicka";
-    inputButton.className = "button"; 
-    boardID.appendChild(inputButton);
-    
-    // Add eventhandlers
-    inputButton.addEventListener("click", this.createMessage, false);
-    
-};
-
-MessageBoard.prototype.createMessage = function(){
-    var input = document.querySelector("textarea").value;
-    var mess = new Message(input, new Date());
-};
 
 MessageBoard.prototype.renderMessage = function(){
     
