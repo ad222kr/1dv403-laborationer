3ebf4775e705b869db9071db475048deda2be742
ad222kr <ad222kr@student.lnu.se>
2014-11-25 15:40:28 +0000

    Now renders a table

--- 3-gameon/memory/script/MemoryBoard.js
+++ 3-gameon/memory/script/MemoryBoard.js
@@ -2,13 +2,43 @@
 
 function MemoryBoard(rows, cols, gameID){
     
+    var that = this;
+    
+    // Gets reference to the gameboard, setting float so boards line up next to eachother
+    var div = document.querySelector("#" + gameID);
+    div.style.float = "left";
+    
+    
+    // Randomizes array for the pictures
     this.tiles = RandomGenerator.getPictureArray(cols, rows);
     
-    
     this.start = function(){
+        // Starts the game
         
-        console.log  (this.tiles);     
+        // Create a header
         
+        this.generateTable();    
+        
+    };
+    
+    this.generateTable = function(){
+        
+        // Generates the table for the game
+        var table = document.createElement("table");
+        var header = document.createElement("h1");
+        div.appendChild(header).innerHTML = "memory";
+        div.appendChild(table);
+        for(var i = 1; i <= rows; i++){
+            
+            var row = document.createElement("tr");
+            table.appendChild(row);
+            
+            for(var j = 1; j <= cols; j++){
+                
+                var cell = document.createElement("td");
+                row.appendChild(cell);
+            }
+        }
     };
     
     
--- 3-gameon/memory/script/memoryapp.js
+++ 3-gameon/memory/script/memoryapp.js
@@ -2,8 +2,8 @@ var MemoryApp = {
     
     init: function(){
         
-        var mem1 = new MemoryBoard(3,4,"game1");
-        var mem2 = new MemoryBoard(2,3,"game2");
+        var mem1 = new MemoryBoard(3,4,"board1");
+        var mem2 = new MemoryBoard(2,3,"board2");
         
         mem1.start();
         mem2.start();
