a4abb3d1f71ebcfbddc24d5121b11e4b8129a11f
ad222kr <ad222kr@student.lnu.se>
2014-11-25 18:38:10 +0000

    trying to turn the tile on click but pretty stuck,..

--- 3-gameon/memory/script/MemoryBoard.js
+++ 3-gameon/memory/script/MemoryBoard.js
@@ -3,43 +3,62 @@
 function MemoryBoard(rows, cols, gameID){
     
     var that = this;
-    
-    // Gets reference to the gameboard, setting float so boards line up next to eachother
+
     var div = document.querySelector("#" + gameID);
-    div.style.float = "left";
+
+    this.tiles = [];
     
-    
-    // Randomizes array for the pictures
-    this.tiles = RandomGenerator.getPictureArray(cols, rows);
     
     this.start = function(){
         // Starts the game
-        
-        // Create a header
-        
-        this.generateTable();    
+        this.tiles = RandomGenerator.getPictureArray(cols, rows);
+        this.generateTable();  
+        console.log(this.tiles);
         
     };
     
     this.generateTable = function(){
-        
         // Generates the table for the game
+        var cellCount = 0;
         var table = document.createElement("table");
         var header = document.createElement("h1");
         div.appendChild(header).innerHTML = "memory";
         div.appendChild(table);
-        for(var i = 1; i <= rows; i++){
+        // Populates each cell with the facedown-image
+        for(var i = 0; i < rows; i++){
             
             var row = document.createElement("tr");
             table.appendChild(row);
             
-            for(var j = 1; j <= cols; j++){
-                
+            for(var j = 0; j < cols; j++){
+                // Creates cells with a-tag that holds an img-tag
                 var cell = document.createElement("td");
+                var a = document.createElement("a");
+                var img = document.createElement("img");
+                a.href = "#";
+                img.src = "pics/0.png";
+                a.appendChild(img);
+                cell.appendChild(a);
                 row.appendChild(cell);
+                a.className = this.tiles[cellCount];
+                cellCount++;
             }
         }
+        
+        // Eventlistener
+        table.addEventListener("click", that.flipTile, false);
     };
+    
+    this.flipTile = function(e){
+        var classname = e.className;
+        
+        console.log(classname);
+        
+        
+    };
+    
+    
+    
     
     
 }
--- 3-gameon/memory/script/memoryapp.js
+++ 3-gameon/memory/script/memoryapp.js
@@ -3,7 +3,7 @@ var MemoryApp = {
     init: function(){
         
         var mem1 = new MemoryBoard(3,4,"board1");
-        var mem2 = new MemoryBoard(2,3,"board2");
+        var mem2 = new MemoryBoard(4,4,"board2");
         
         mem1.start();
         mem2.start();
