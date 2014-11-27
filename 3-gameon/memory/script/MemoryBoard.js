09d55681add238519cc21c3bfc1603545c0e5b6e
ad222kr <ad222kr@student.lnu.se>
2014-11-27 23:44:47 +0100

    Copy paste code from earlier commit, forgot to pull repo when i got home...

--- 3-gameon/memory/script/MemoryBoard.js
+++ 3-gameon/memory/script/MemoryBoard.js
@@ -10,10 +10,6 @@ function MemoryBoard(rows, cols, gameID){
     var flippedCount = 0;
     var flippedArr = [];
     var numberOfTries = 0;
-<<<<<<< HEAD
-
-=======
->>>>>>> d87bd486ce95ecaf8e1b7ca9576a861ac0d218c2
     this.tiles = [];
 
     this.start = function(){
@@ -22,52 +18,20 @@ function MemoryBoard(rows, cols, gameID){
         this.generateTable();
 
     };
-<<<<<<< HEAD
-
-=======
->>>>>>> d87bd486ce95ecaf8e1b7ca9576a861ac0d218c2
     this.flipTile = function(e){
         // Function for flipping a card
 
         var target = e.target;
-<<<<<<< HEAD
-
-        // Loops through the tiles array to check if the clicked picture classname
-        // matches with any position. When it finds a match, it sets src to the appropriate picture
-
-=======
->>>>>>> d87bd486ce95ecaf8e1b7ca9576a861ac0d218c2
 
         if (!target.classList.contains("clicked" && flippedCount < 2)){
             // Loops through the tiles array to check if the clicked picture classname
             // matches with any position. When it finds a match, it sets src to the appropriate picture
             for(var i = 0; i <= this.tiles.length; i++){
 
-<<<<<<< HEAD
-            if(target.className == this.tiles[i]){
-
-                target.src = "pics/" + this.tiles[i] + ".png";
-            }
-
-        }
-
-        // Pushed the image to an array that will hold the two clicked
-        // to compare their classnames to find a match
-        // asD
-        if(flippedCount <= 2){
-
-            // prevents pushing to the array if the target has already been clicked
-            if (!target.classList.contains("clicked")){
-
-                flippedArr.push(target);
-                target.classList.add("clicked");
-                flippedCount++;
-=======
                 if(target.className == this.tiles[i]){
 
                     target.src = "pics/" + this.tiles[i] + ".png";
                 }
->>>>>>> d87bd486ce95ecaf8e1b7ca9576a861ac0d218c2
             }
             // Pushed the image to an array that will hold the two clicked
             // to compare their classnames to find a match
@@ -92,12 +56,8 @@ function MemoryBoard(rows, cols, gameID){
 
             numberOfMatches++;
 
-<<<<<<< HEAD
-            // Sets classname of the matched
-=======
             // Sets classname of the matched to pair to prevent
             // user from clicking those images again
->>>>>>> d87bd486ce95ecaf8e1b7ca9576a861ac0d218c2
             for(var i = 0; i < flippedArr.length; i++){
                 flippedArr[i].className = "pair";
             }
@@ -119,14 +79,7 @@ function MemoryBoard(rows, cols, gameID){
         }
 
         // checks if victory, probably move this to own function later
-<<<<<<< HEAD
-        if(numberOfMatches == maxNumberOfMatches){
-
-            alert("VICTORY");
-        }
-=======
         this.checkVictory();
->>>>>>> d87bd486ce95ecaf8e1b7ca9576a861ac0d218c2
     };
 
     this.generateTable = function(){
@@ -162,28 +115,24 @@ function MemoryBoard(rows, cols, gameID){
         table.addEventListener("click", function(e){ that.clickFunction(e); });
     };
 
-<<<<<<< HEAD
-
-=======
->>>>>>> d87bd486ce95ecaf8e1b7ca9576a861ac0d218c2
     this.clickFunction = function(e){
 
         if (!e){ e = window.event}
 
-        // Checks for tagname so flipTale is only called when img is clicked
-        // since eventlistener is on the whole table.
-        if(e.target.tagName == "IMG" && e.target.className != "pair" && flippedCount < 2){
-            that.flipTile(e);
-        }
-    };
+            // Checks for tagname so flipTale is only called when img is clicked
+            // since eventlistener is on the whole table.
+            if(e.target.tagName == "IMG" && e.target.className != "pair" && flippedCount < 2){
+                that.flipTile(e);
+            }
+        };
 
-    this.checkVictory = function(){
+        this.checkVictory = function(){
 
-        if(numberOfMatches == maxNumberOfMatches){
-            var p = document.createElement("p")
-            p.innerHTML = "Grattis! Det tog dig "+numberOfTries+" att klara spelet!";
-            div.appendChild(p);
+            if(numberOfMatches == maxNumberOfMatches){
+                var p = document.createElement("p")
+                p.innerHTML = "Grattis! Det tog dig "+numberOfTries+" att klara spelet!";
+                div.appendChild(p);
+            }
         }
+
     }
-
-}
