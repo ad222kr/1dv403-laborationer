7cc8e18fdd302274671870697fdf9f1672dd3b41
ad222kr <ad222kr@student.lnu.se>
2014-11-28 09:59:47 +0100

    Pushing

--- 2-labbymezzage/js/MessageBoard.js
+++ 2-labbymezzage/js/MessageBoard.js
@@ -3,9 +3,9 @@ function MessageBoard(boardID){
     var that = this;
     // getting reference to the board
     var div = document.querySelector("#" + boardID);
-    
+
     this.messages = [];
-    
+
     this.init = function(){
         // Init function that initiates the board, creating a button and a textfield
         // Creating div to hold the messages
@@ -17,19 +17,19 @@ function MessageBoard(boardID){
         var countDiv = document.createElement("div");
         countDiv.className = "numberofmess";
         div.appendChild(countDiv);
-        
+
         // Create a text-field
         var textarea = document.createElement("textarea");
         textarea.className = "textarea";
         div.appendChild(textarea);
-        
+
         // Create a send-button
         var inputButton = document.createElement("input");
         inputButton.type = "button";
         inputButton.value = "skicka";
         inputButton.className = "button";
         div.appendChild(inputButton);
-        
+
         // Add eventlisteners
         inputButton.addEventListener("click", that.createMessage, false);
         textarea.addEventListener("keypress", function(e) {
@@ -38,40 +38,40 @@ function MessageBoard(boardID){
                 e.preventDefault();
                 that.createMessage();
             }
-            
+
         });
     };
-    
+
     this.createMessage = function(){
         // Reference to the textarea
         var input = document.querySelector("#"+boardID+" textarea");
-        
+
         var mess = new Message(input.value, new Date());
-        
+
         // Adds message to array and renders it
-        that.messages.push(mess); 
+        that.messages.push(mess);
         that.renderMessage(that.messages.length - 1);
         // Resets the text-field on click/enter
         input.value= "";
         that.messageCount();
-        
+
         // that in this func cus when i click "this" points to input.button even tho
         // the eventhandler uses that. it works with eventlistener for enter key tho??
     };
-    
+
     this.renderMessage = function(messageID){
         var messagesDiv = document.querySelector("#" +boardID+" .messages");
-        
+
         // Creating div for the message object
         var messageDiv = document.createElement("div");
         messageDiv.className = "message";
         messagesDiv.appendChild(messageDiv);
-        
+
         // Creating a div to hold the buttons for timestamp/delete
         var buttonDiv = document.createElement("div");
         buttonDiv.className = "buttons";
         messageDiv.appendChild(buttonDiv);
-        
+
         // Creating the delete-button
         var aClose = document.createElement("a");
         var imgClose = document.createElement("img");
@@ -80,7 +80,7 @@ function MessageBoard(boardID){
         imgClose.setAttribute("src", "img/Remove-icon.png");
         aClose.appendChild(imgClose);
         buttonDiv.appendChild(aClose);
-        
+
         // Creating the timestamp-button
         var aTime = document.createElement("a");
         var imgTime = document.createElement("img");
@@ -89,21 +89,21 @@ function MessageBoard(boardID){
         imgTime.setAttribute("src", "img/clock.png");
         aTime.appendChild(imgTime);
         buttonDiv.appendChild(aTime);
-        
+
         // Creating elements and rendering the message
         var text = document.createElement("p");
         text.className = "msgtext";
         messageDiv.appendChild(text);
-        
+
         // Creating elements and rendering the timestamp
         var time = document.createElement("p");
         time.className = "timestamp";
         messageDiv.appendChild(time);
-        
+
         // Rendering the message and time
         text.innerHTML = this.messages[messageID].getHTMLtext();
         time.innerHTML = this.messages[messageID].getDateText();
-        
+
         // Eventhandlers for the buttons
         imgClose.alt="close";
         imgClose.addEventListener("click", function(e) {
@@ -115,18 +115,18 @@ function MessageBoard(boardID){
         imgTime.addEventListener("click", function(e) {
             //that.showTimeStamp(messageID);
             that.showTimeStamp(messageID);
-            
+
         });
-        
+
         this.renderMessages = function(){
             // Clears the messages and renders them all again
             document.querySelector("#" + boardID+ " .messages").innerHTML="";
-            
+
             for(var i = 0; i < that.messages.length; i++){
                 that.renderMessage(i);
             }
         };
-        
+
         this.deleteMessage = function(messageID){
             // Deletets one message then calls renderMessages to render all
             if(window.confirm("Vill du verkligen ta bort meddelandet?")){
@@ -134,18 +134,18 @@ function MessageBoard(boardID){
                 this.renderMessages();
             }
         };
-        
+
         this.showTimeStamp = function(messageID){
             alert(this.messages[messageID].toString());
         };
-        
+
         this.messageCount = function(messageID){
             var count = this.messages.length;
             var div = document.querySelector("#" +boardID +" .numberofmess");
-            
+
             // Removes messagecounter if all messages are deleted
             if(this.messages.length !== 0){
-                div.innerHTML = "Antal meddelanden: " + count;    
+                div.innerHTML = "Antal meddelanden: " + count;
             }
             else{
                 div.innerHTML = "";
@@ -153,6 +153,3 @@ function MessageBoard(boardID){
         };
     };
 }
-
-
-
