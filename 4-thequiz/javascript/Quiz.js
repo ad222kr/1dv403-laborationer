41e66d059b2d65b29f0e89760900f23bdd6322ba
ad222kr <ad222kr@student.lnu.se>
2014-12-10 02:58:00 +0100

    Indentation messing around

--- 4-thequiz/javascript/Quiz.js
+++ 4-thequiz/javascript/Quiz.js
@@ -1,7 +1,7 @@
 "use strict";
 
 var Quiz = {
-	// TODO: Prevent user from clicking send if question already answered, remove eventlisteners maybe?
+    // TODO: Prevent user from clicking send if question already answered, remove eventlisteners maybe?
 
     div : document.getElementById("quiz"),
 
@@ -15,66 +15,66 @@ var Quiz = {
     },
 
     getRequest: function(xhr, url){
-    	// Get request from server, prints the question
-    	// And sets Quiz.URL to url for getting the answer
+        // Get request from server, prints the question
+        // And sets Quiz.URL to url for getting the answer
 
         xhr.onreadystatechange = function(){
             if(xhr.readyState === 4 ){
                 if(xhr.status === 200 || xhr.status === 304){
-                	
-            	 	Quiz.printQuestion(JSON.parse(xhr.responseText));
-            	 	Quiz.URL = 	JSON.parse(xhr.responseText).nextURL;
-            	 	console.log(Quiz.URL);
-    
+
+                    Quiz.printQuestion(JSON.parse(xhr.responseText));
+                    Quiz.URL =      JSON.parse(xhr.responseText).nextURL;
+                    console.log(Quiz.URL);
+
                 }
                 else{
-                	console.log("Läsfel, status: "+xhr.status);
+                    console.log("Läsfel, status: "+xhr.status);
                 }   
             }           
         };
-        
+
         Quiz.xhr.open("GET", url, true);        
         Quiz.xhr.send(null);
     },
 
- 	sendRequest: function(input, url, xhr){
- 		// Sent to server, if status 400 URL now still stays the same (for answer)
- 		// so user can try again until they get the right answer
-		xhr.onreadystatechange = function(){
-	        if(xhr.readyState === 4 ){
-	            if(xhr.status != 400 ){
-	            	document.querySelector(".statustext").innerHTML = "du svarade rätt";
-	        	 	Quiz.newQuestion(JSON.parse(Quiz.xhr.responseText).nextURL); 
-	            }
-	            else{
-	            	document.querySelector(".statustext").innerHTML = "du svarade fel, försök igen";
-	            	console.log("Läsfel, status: "+xhr.status);
-	            }   
-	        }	        
-	    };
-		// Post the stringified object to server
-	    xhr.open("POST", url, true);
-	    xhr.setRequestHeader("Content-Type", "application/json");
-	    var sendObject = { answer: input.value }	    
-	    xhr.send(JSON.stringify(sendObject));    
-	},
+    sendRequest: function(input, url, xhr){
+        // Sent to server, if status 400 URL now still stays the same (for answer)
+        // so user can try again until they get the right answer
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState === 4 ){
+                if(xhr.status != 400 ){
+                    document.querySelector(".statustext").innerHTML = "du svarade rätt";
+                    Quiz.newQuestion(JSON.parse(Quiz.xhr.responseText).nextURL); 
+                }
+                else{
+                    document.querySelector(".statustext").innerHTML = "du svarade fel, försök igen";
+                    console.log("Läsfel, status: "+xhr.status);
+                }   
+            }               
+        };
+        // Post the stringified object to server
+        xhr.open("POST", url, true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        var sendObject = { answer: input.value }        
+        xhr.send(JSON.stringify(sendObject));    
+    },
 
     newQuestion: function(url){
-    	// Shows link for getting to next question when user inputs correct answer
-    	var a = document.querySelector(".nextQuestion");
-    	var status = document.querySelector(".statustext");
-    	
-    	a.innerHTML = "Nästa Fråga";
-    	a.addEventListener("click", function(){
-    		Quiz.getRequest(Quiz.xhr, url);
-    		a.innerHTML = "";
-    		status.innerHTML = "";
-    	});
-    	
+        // Shows link for getting to next question when user inputs correct answer
+        var a = document.querySelector(".nextQuestion");
+        var status = document.querySelector(".statustext");
+
+        a.innerHTML = "Nästa Fråga";
+        a.addEventListener("click", function(){
+            Quiz.getRequest(Quiz.xhr, url);
+            a.innerHTML = "";
+            status.innerHTML = "";
+        });
+
     },
-    
+
     printQuestion: function(response){
-    	// Prints the question (lol)
+        // Prints the question (lol)
         var qField = document.querySelector(".questionField");
         var qHeader = document.querySelector(".questionHeader");
         qHeader.innerHTML = "Fråga nummer: " + response.id;
@@ -82,25 +82,25 @@ var Quiz = {
 
     },
     correctAnswer: function(){
-    	// TODO: refactor correct answer to this func
+        // TODO: refactor correct answer to this func
 
-	},
+    },
 
-	wrongAnster: function(){
-		// TODO: refactor wrong answer to this func
+    wrongAnster: function(){
+        // TODO: refactor wrong answer to this func
 
-	},
+    },
 
-	victory: function(){
-		// TODO: function to remove questionfield, button etc when all q's done
-		// and print out a table, maybe number of tries per question and a
-		// congratulation
+    victory: function(){
+        // TODO: function to remove questionfield, button etc when all q's done
+        // and print out a table, maybe number of tries per question and a
+        // congratulation
 
-	},
+    },
 
     buildBasicElements: function(div){
 
-    	// Refactor this into smaller funcs?
+        // Refactor this into smaller funcs?
 
         // Create question field
         var qDiv = document.createElement("div");
@@ -137,18 +137,17 @@ var Quiz = {
         statusDiv.appendChild(statusMessage);
 
         // a-tag for next question link
-    	var a = document.createElement("a");
-    	div.appendChild(a);
-    	a.className = "nextQuestion";
-    	a.href = "#";
+        var a = document.createElement("a");
+        div.appendChild(a);
+        a.className = "nextQuestion";
+        a.href = "#";
 
         // Eventhandlers
         inputButton.addEventListener("click", function(){
-        	if (inputText.value != ""){
-        		Quiz.sendRequest(inputText, Quiz.URL, Quiz.xhr);
-            	inputText.value = "";	
-        	}
-            
+            if (inputText.value != ""){
+            Quiz.sendRequest(inputText, Quiz.URL, Quiz.xhr);
+            inputText.value = "";   
+            }
         });
     }
 
