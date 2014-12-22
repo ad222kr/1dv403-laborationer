75098acdae93303005223b4456fad4a0beddc656
ad222kr <ad222kr@student.lnu.se>
2014-12-22 19:27:50 +0100

    Basic structure for project is almost done

---
+++ 5-PWD/script/lib/modules/desktop.js
@@ -0,0 +1,10 @@
+"use strict";
+
+define(
+	["Mustache", "Window", "jquery"],
+	function($, Window, Apps, jquery){
+
+		console.log("Tjena");
+
+	}
+)
---
+++ 5-PWD/script/main.js
@@ -0,0 +1,19 @@
+"uses strict";
+
+require.config({
+	baseUrl: 'script/lib',
+
+	paths: {
+		Mustache: 'mustache',
+		Window: 'modules/window/window',
+		Apps: 'modules/apps',
+		jquery: 'jquery-2.1.3'
+	}
+});
+
+require(["modules/desktop"], function(desktop){
+	//This function is called when scripts/lib/modules/desktop.js is loaded.
+    //If desktop.js calls define(), then this function is not fired until
+    //desktops's dependencies have loaded, and the desktop argument will hold
+    //the module value for "modules/desktop".
+});
